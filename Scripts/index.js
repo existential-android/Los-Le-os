196850bcce71b7f91367907f6d587f843c9e5a05
Manuel Ortiz <gantz081201@gmail.com>
2021-09-24 16:42:01 -0500

    home de los leños

---
+++ Scripts/index.js
@@ -0,0 +1,12 @@
+let menu_principal = document.querySelector(".navegacion__contenedor")
+let abrir_menu = document.querySelector(".abrirMenu")
+let cerrar_menu = document.querySelector(".cerrarMenu")
+
+abrir_menu.addEventListener('click', abrirMenu);
+cerrar_menu.addEventListener('click', cerrarMenu);
+function abrirMenu(){
+    menu_principal.style.top = "0";
+}
+function cerrarMenu(){
+    menu_principal.style.top = "-100%";
+}
